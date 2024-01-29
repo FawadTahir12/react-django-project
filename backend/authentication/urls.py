6105363d6c8061687798abe29ec954a99cfb84b3
fawad <fawadtahir007@gmail.com>
2024-01-29 22:16:56 +0500

    Testing

--- backend/authentication/urls.py
+++ backend/authentication/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('register/', CustomUserCreate.as_view()),
     path('login/google/', GoogleLoginApi.as_view(),  name="login-with-google"),
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('send-reset-password-mail/', SendPasswordRecoveryEmail.as_view(), name='send_password_reset_mail'),
     path('reset-password/', resetpassword.as_view(), name='reset_password'),
 ]
